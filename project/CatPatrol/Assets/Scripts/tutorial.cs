fc54b57d180c466be02f67cbcc40247514fb1c19
Eli <smitel06@hotmail.com>
2021-07-07 12:03:55 +0930

    tutorial + gingy boy added

---
+++ project/CatPatrol/Assets/Scripts/tutorial.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tutorial : MonoBehaviour
+{
+
+    //cursor stuff
+    public Texture2D cursorTexture;
+    public Texture2D defaultTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotspot = Vector2.zero;
+    //tutorial go
+    public GameObject tutorialScreen;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void clicked()
+    {
+        //starts the game
+        Time.timeScale = 1;
+        Destroy(tutorialScreen);
+    }
+
+    public void mouseOver()
+    {
+        //set cursor for mouse
+        Cursor.SetCursor(cursorTexture, hotspot, cursorMode);
+    }
+
+    public void mouseExit()
+    {
+        Cursor.SetCursor(defaultTexture, hotspot, cursorMode);
+    }
+}
